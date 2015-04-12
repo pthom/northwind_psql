ed49de8dab4527383f049b580e78f6c1726a401f
Pascal Thomet <pthomet@ivsweb.com>
2015-04-12 12:00:42 +0200

    +create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dropdb northwind
+dropuser northwind_user
+
+createdb northwind
+psql northwind < northwind.sql
+
+psql template1 -c "create user northwind_user;"
+psql template1 -c "alter user northwind_user password 'thewindisblowing';"
+psql template1 -c "grant all on DATABASE northwind to northwind_user;"
