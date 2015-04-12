f93f028f37adc4d81140088dbb35196b70ba62ad
Pascal Thomet <pthomet@ivsweb.com>
2015-04-12 12:25:21 +0200

    grant

--- create_db.sh
+++ create_db.sh
@@ -9,3 +9,4 @@ psql northwind < northwind.sql
 psql template1 -c "create user northwind_user;"
 psql template1 -c "alter user northwind_user password 'thewindisblowing';"
 psql template1 -c "grant all on DATABASE northwind to northwind_user;"
+psql northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
