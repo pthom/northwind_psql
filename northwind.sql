50173f0d744b4d9adea1d1c3aa934698510c060f
larryq <lquantz@gmail.com>
2018-01-10 14:13:05 -0800

    Update northwind.sql
    removed sections that needed root access, so unable to run script on hosted solutions like elephantsql-- didn't seem to prevent the tables from being created.

--- northwind.sql
+++ northwind.sql
@@ -9,18 +9,7 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
 
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET default_tablespace = '';
 
