0b7ba098c51fe9f469d77bf19d25bc60d39672cd
Pascal Thomet <pthomet@gmail.com>
2019-05-04 13:07:42 +0200

    use --no-psqlrc to ensure basic use of psql

--- create_db.sh
+++ create_db.sh
@@ -6,7 +6,7 @@ dropuser --if-exists northwind_user
 createdb northwind
 psql northwind < northwind.sql
 
-psql template1 -c "create user northwind_user;"
-psql template1 -c "alter user northwind_user password 'thewindisblowing';"
-psql template1 -c "grant all on DATABASE northwind to northwind_user;"
-psql northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
+psql --no-psqlrc template1 -c "create user northwind_user;"
+psql --no-psqlrc template1 -c "alter user northwind_user password 'thewindisblowing';"
+psql --no-psqlrc template1 -c "grant all on DATABASE northwind to northwind_user;"
+psql --no-psqlrc northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
