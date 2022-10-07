4aa7967a33b84e693db56d4c23ada410a3ff04a6
Pascal Thomet <pthomet@gmail.com>
2022-10-07 08:33:27 +0200

    Change bpchar to varchar / thanks @valhuber

--- northwind.sql
+++ northwind.sql
@@ -53,8 +53,8 @@ CREATE TABLE categories (
 --
 
 CREATE TABLE customer_customer_demo (
-    customer_id bpchar NOT NULL,
-    customer_type_id bpchar NOT NULL
+    customer_id character varying(5) NOT NULL,
+    customer_type_id character varying(5) NOT NULL
 );
 
 
@@ -63,7 +63,7 @@ CREATE TABLE customer_customer_demo (
 --
 
 CREATE TABLE customer_demographics (
-    customer_type_id bpchar NOT NULL,
+    customer_type_id character varying(5) NOT NULL,
     customer_desc text
 );
 
@@ -73,7 +73,7 @@ CREATE TABLE customer_demographics (
 --
 
 CREATE TABLE customers (
-    customer_id bpchar NOT NULL,
+    customer_id character varying(5) NOT NULL,
     company_name character varying(40) NOT NULL,
     contact_name character varying(30),
     contact_title character varying(30),
@@ -144,7 +144,7 @@ CREATE TABLE order_details (
 
 CREATE TABLE orders (
     order_id smallint NOT NULL,
-    customer_id bpchar,
+    customer_id character varying(5),
     employee_id smallint,
     order_date date,
     required_date date,
@@ -184,7 +184,7 @@ CREATE TABLE products (
 
 CREATE TABLE region (
     region_id smallint NOT NULL,
-    region_description bpchar NOT NULL
+    region_description character varying(60) NOT NULL
 );
 
 
@@ -226,7 +226,7 @@ CREATE TABLE suppliers (
 
 CREATE TABLE territories (
     territory_id character varying(20) NOT NULL,
-    territory_description bpchar NOT NULL,
+    territory_description character varying(60) NOT NULL,
     region_id smallint NOT NULL
 );
 
